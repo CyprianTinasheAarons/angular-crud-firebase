3ad1c54a3b19e87c531bd6eb0d60afac41bb751c
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2021-09-19 23:07:10 +0200

    Remove environments key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,4 @@
 export const environment = {
   production: true,
-      apiKey: "AIzaSyD-TQvuuPzUsfkrn8OPJaxaI21APEQMA9s",
-      authDomain: "tutorials-3f7a6.firebaseapp.com",
-      databaseURL: "https://tutorials-3f7a6-default-rtdb.firebaseio.com",
-      projectId: "tutorials-3f7a6",
-      storageBucket: "tutorials-3f7a6.appspot.com",
-      messagingSenderId: "726685097003",
-      appId: "1:726685097003:web:8225fe49409d623d41de6e"
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-      apiKey: "AIzaSyD-TQvuuPzUsfkrn8OPJaxaI21APEQMA9s",
-      authDomain: "tutorials-3f7a6.firebaseapp.com",
-      databaseURL: "https://tutorials-3f7a6-default-rtdb.firebaseio.com",
-      projectId: "tutorials-3f7a6",
-      storageBucket: "tutorials-3f7a6.appspot.com",
-      messagingSenderId: "726685097003",
-      appId: "1:726685097003:web:8225fe49409d623d41de6e"
+
 
   }
 };
